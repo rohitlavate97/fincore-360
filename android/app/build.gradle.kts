c350fea808907211022ae05fa086b42a98c41650
Rohit Lavate <rohitlavate97@gmail.com>
2026-08-29 14:59:29 +0530

    feat(android): add network_security_config disabling cleartext and configure release signing (H-6 / Section 8 Item 10)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,6 +22,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
